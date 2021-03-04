939df8713525f330176a5405ca3ed17824d94cf8
MatiasJoseGarciaFernandez <matiasjose.garcia.alu@iescampanillas.com>
2021-03-04 18:58:09 +0100

    cambios

--- IDE´S/src/prueba.java
+++ IDE´S/src/prueba.java
@@ -2,5 +2,6 @@
 public class prueba {
 	public static void main(String[] args) {
 		System.out.print("trabajo de los IDES, Matias Jose Garcia Fernandez");
+		System.out.println("cambio local");
 	}
 }
